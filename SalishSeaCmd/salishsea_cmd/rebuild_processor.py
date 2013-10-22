b308c0894375158f0caee3c4a8aa5c3e3aa03fa9
Doug Latornell <djl@douglatornell.ca>
2013-10-21 17:45:07 -0700

    Implement rebuild processor.

--- SalishSeaCmd/salishsea_cmd/rebuild_processor.py
+++ SalishSeaCmd/salishsea_cmd/rebuild_processor.py
@@ -19,8 +19,10 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 from __future__ import absolute_import
+import glob
 import logging
 import os
+import subprocess
 import sys
 from . import utils
 
@@ -30,23 +32,41 @@ __all__ = ['main']
 
 log = logging.getLogger('rebuild')
 log.setLevel(logging.DEBUG)
+log.addHandler(utils.make_stdout_logger)
 log.addHandler(utils.make_stderr_logger())
 
 
 def main(run_desc):
-    print(run_desc)
-    rebuild_nemo_exec = _find_rebuild_nemo_exec(
-        run_desc['paths']['NEMO-code'])
+    rebuild_nemo_exec = _find_rebuild_nemo_exec(run_desc['paths']['NEMO-code'])
+    name_roots, ncores = _get_results_files()
+    for fn in name_roots:
+        result = subprocess.check_output(
+            [rebuild_nemo_exec, fn, str(ncores)],
+            stderr=subprocess.STDOUT,
+            universal_newlines=True)
+        log.info(result)
 
 
 def _find_rebuild_nemo_exec(nemo_code_path):
     rebuild_nemo_exec = os.path.join(
         nemo_code_path,
-        'NEMOGCM/TOOLS/REBUILD_NEMO/rebuild_nemo.exe',
-    )
+        'NEMOGCM/TOOLS/REBUILD_NEMO/rebuild_nemo.exe')
     if not os.path.lexists(rebuild_nemo_exec):
         log.error(
             'Error: {} not found - did you forget to build it?'
             .format(rebuild_nemo_exec))
         sys.exit(2)
-    return rebuild_nemo_exec
+    rebuild_nemo_script = os.path.splitext(rebuild_nemo_exec)[0]
+    return rebuild_nemo_script
+
+
+def _get_results_files():
+    result_pattern = '*_0000.nc'
+    name_roots = [fn[:-8] for fn in glob.glob(result_pattern)]
+    if not name_roots:
+        log.error(
+            'Error: no files found that match the {} pattern'
+            .format(result_pattern))
+        sys.exit(2)
+    ncores = len(glob.glob(name_roots[0] + '_[0-9][0-9][0-9][0-9].nc'))
+    return name_roots, ncores
