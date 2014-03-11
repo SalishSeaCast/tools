8a4b2334dd123cf93d5dfa31fef5f4236ccaae6c
Doug Latornell <djl@douglatornell.ca>
2014-03-11 22:18:55 +0100

    Add newlines between list elements.

--- bathymetry/make_readme.py
+++ bathymetry/make_readme.py
@@ -31,12 +31,11 @@ and visualizing bathymetry netCDF files.
 
 """
 for fn in (fn for fn in os.listdir('./') if fn.endswith('ipynb')):
-    readme += '* [{fn}]({url}/{fn})  \n'.format(fn=fn, url=url)
+    readme += '* [{fn}]({url}/{fn})  \n    \n'.format(fn=fn, url=url)
     with open(fn, 'rt') as notebook:
         contents = json.load(notebook)
     if contents['worksheets'][0]['cells'][0]['cell_type'] == 'markdown':
         desc_lines = contents['worksheets'][0]['cells'][0]['source']
-        readme += '    \n'
         for line in desc_lines:
             if title_pattern.match(line):
                 line = title_pattern.sub('    **', line)
