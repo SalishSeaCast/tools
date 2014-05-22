0a4a76b5e9ddebc8c86bcc5571903d04ce8ba06a
Doug Latornell <djl@douglatornell.ca>
2014-05-22 12:14:37 -0700

    Change results files compression to a per-subcommand option.
    `combine` and `gather` will retain their default behaviour of compressing
    results files, disablable via the `--no-compress` option.
    `run` will not compress by default, but have a `--compress` option to enable
    compression.

--- SalishSeaCmd/salishsea_cmd/gather.py
+++ SalishSeaCmd/salishsea_cmd/gather.py
@@ -51,6 +51,9 @@ class Gather(cliff.command.Command):
 
             If RESULTS_DIR does not exist it will be created.
         '''
+        parser.add_argument(
+            '--no-compress', action='store_true',
+            help="don't compress results files")
         lib.add_combine_gather_options(parser)
         return parser
 
