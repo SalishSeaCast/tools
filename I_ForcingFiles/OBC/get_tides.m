9b889b5052167338deca4ad0f8358e1f3841dc03
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2014-06-24 14:59:05 -0700

    Updated description to say we are using t_tide instead of t_xtide.

--- I_ForcingFiles/OBC/get_tides.m
+++ I_ForcingFiles/OBC/get_tides.m
@@ -3,13 +3,13 @@ function [pred,wlev,anomaly,tim] = get_tides(csvfilename, location)
 % Take the csv file of measured water level and calculate sea surface
 % anomaly using predicted tide level from t_xtide
 % e.g. [pred,wlev,anomaly] = get_tides('wlev_timeseries.csv', 'tofino(2)');
-% location is the location for t_xtide predictions. This should correspond
+% location is the location for t_tide predictions. This should correspond
 % to the location of the measured water level.
 % The dates are set by the date in the csvfilename
 
 
 % KLS November 2013
-% Feb 2014: This has been adapted to use the t_xtide package. NKS
+% Feb 2014: This has been adapted to use the t_tide package. NKS
 % This fcuntion will save the harmonics data and the predictions in separate files. 
 
 %Read in the measured water level data the location
