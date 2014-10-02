5c8e3030b3d9c024ce90df54c175f48e518067f9
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2014-10-02 15:55:01 -0700

    Added Pam Rocks to get_EC_observations

--- SalishSeaTools/salishsea_tools/stormtools.py
+++ SalishSeaTools/salishsea_tools/stormtools.py
@@ -224,6 +224,7 @@ def get_EC_observations(station, start_day, end_day):
 
     """
     station_ids = {
+	'PamRocks': 6817,
 	'EntranceIsland': 29411,
         'Sandheads': 6831,
         'YVR': 889,
