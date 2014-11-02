d6ac3249ba4e172ac6e1014975285d08c1b26b0c
Susan Allen <sallen@eoas.ubc.ca>
2014-11-02 13:50:11 -0800

    Move files to nowcast.  Make links absolute so anyone can run.

--- SalishSeaTools/salishsea_tools/nowcast/deflate.sh
+++ SalishSeaTools/salishsea_tools/nowcast/deflate.sh
@@ -1,8 +1,10 @@
 # deflate file
-cd ../../../Operational
+cd /ocean/sallen/allen/research/MEOPAR/Operational
+
 Year=$(date +%Y)
 Month=$(date +%m)
 Day=$(date +%d)
 netfile=ops_y${Year}m${Month}d${Day}.nc
+
 echo $netfile
 ncks -4 -L4 -O $netfile $netfile
