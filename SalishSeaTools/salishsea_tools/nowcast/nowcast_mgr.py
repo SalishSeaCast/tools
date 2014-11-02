cd62c8deb86c26b95b1cff7ff4d09cc42d50a16e
Doug Latornell <djl@douglatornell.ca>
2014-11-02 06:40:51 -0800

    Start development of nowcast system manager & worker scripts.

---
+++ SalishSeaTools/salishsea_tools/nowcast/nowcast_mgr.py
@@ -0,0 +1,82 @@
+# Copyright 2013-2014 The Salish Sea MEOPAR contributors
+# and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Salish Sea NEMO nowcast manager.
+"""
+import logging
+import os
+import sys
+
+import yaml
+import zmq
+
+
+logger = logging.getLogger('nowcast_mgr')
+
+context = zmq.Context()
+
+
+def main(args):
+    config_file = args[0]
+    config = load_config(config_file)
+    configure_logging(config, logger)
+    logger.info('running in process {}'.format(os.getpid()))
+    logger.info('read config from {}'.format(config_file))
+    socket = init_req_rep(config['ports']['req_rep'], context)
+    while True:
+        try:
+            message = socket.recv()
+        except KeyboardInterrupt:
+            logger.info('keyboard interrupt received; shutting down')
+            context.destroy()
+            break
+        logger.info('REQ:{}'.format(message))
+        reply = parse_message(message)
+        socket.send(reply)
+
+
+def load_config(config_file):
+    with open(config_file, 'rt') as f:
+        config = yaml.load(f)
+    return config
+
+
+def configure_logging(config, logger_name):
+    logger.setLevel(logging.DEBUG)
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(
+        '%(asctime)s %(levelname)s [%(name)s] %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+
+def init_req_rep(port, context):
+    socket = context.socket(zmq.REP)
+    socket.bind('tcp://*:{}'.format(port))
+    logger.info('listening for REQs on port {}'.format(port))
+    return socket
+
+
+def parse_message(message):
+    if message.startswith('river data kickoff'):
+        logger.info('launch river data daily average worker')
+        reply = b'ACK'
+    return reply
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
