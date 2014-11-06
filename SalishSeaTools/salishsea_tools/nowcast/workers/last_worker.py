56d5e1272cf24252773a3d811d4fc38a10a4cb4f
Doug Latornell <djl@douglatornell.ca>
2014-11-06 03:37:52 -0800

    Add log file rotation to nowcast mgr & a dev/test worker to trigger it.

---
+++ SalishSeaTools/salishsea_tools/nowcast/workers/last_worker.py
@@ -0,0 +1,55 @@
+# Copyright 2013-2014 The Salish Sea MEOPAR contributors
+# and The University of British Columbia
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#    http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Salish Sea NEMO nowcast worker that sends `end of nowcast` message;
+just for development and testing.
+"""
+import logging
+import os
+
+import zmq
+
+from salishsea_tools.nowcast import lib
+
+
+worker_name = lib.get_module_name()
+
+logger = logging.getLogger(worker_name)
+
+context = zmq.Context()
+
+
+def main():
+    parser = lib.basic_arg_parser(worker_name, description=__doc__)
+    parsed_args = parser.parse_args()
+    config = lib.load_config(parsed_args.config_file)
+    lib.configure_logging(config, logger, parsed_args.debug)
+    logger.info('running in process {}'.format(os.getpid()))
+    logger.info('read config from {.config_file}'.format(parsed_args))
+    lib.install_signal_handlers(logger, context)
+    socket = lib.init_zmq_req_rep_worker(context, config, logger)
+    message = lib.serialize_message(worker_name, 'end of nowcast')
+    socket.send(message)
+    logger.info('sent "end of nowcast" message')
+    msg = socket.recv()
+    message = lib.deserialize_message(msg)
+    logger.info(
+        'received message from {source}: {msg_type}'.format(**message))
+    context.destroy()
+    logger.info('task completed; shutting down')
+
+
+if __name__ == '__main__':
+    main()
