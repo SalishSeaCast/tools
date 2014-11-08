7c614762742ad5ca8905530b632e5e55a2394b90
Doug Latornell <djl@douglatornell.ca>
2014-11-08 11:05:06 -0800

    Rename weather_download worker to download_worker.

--- SalishSeaTools/salishsea_tools/nowcast/workers/weather_18_download.cron.sh
+++ SalishSeaTools/salishsea_tools/nowcast/workers/weather_18_download.cron.sh
@@ -3,9 +3,9 @@
 # usage:
 #   MEOPAR=/data/dlatorne/MEOPAR
 #   NOWCAST_TOOLS=tools/SalishSeaTools/salishsea_tools/nowcast
-#   15 9 * * *  ${MEOPAR}/${NOWCAST_TOOLS}/workers/weather_06_download.cron.sh
+#   15 9 * * *  ${MEOPAR}/${NOWCAST_TOOLS}/workers/weather_18_download.cron.sh
 
 PYTHON=/home/dlatorne/anaconda/bin/python
 NOWCAST=/data/dlatorne/MEOPAR/nowcast
 CONFIG=${NOWCAST}/nowcast.yaml
-${PYTHON} -m salishsea_tools.nowcast.workers.weather_download ${CONFIG} 18
+${PYTHON} -m salishsea_tools.nowcast.workers.download_weather ${CONFIG} 18
