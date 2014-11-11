b252f7c655593c2455a6a1699624b6f2658a406d
Nancy Soontiens <9750865+nsoontie@users.noreply.github.com>
2014-11-10 18:48:41 -0800

    Script to calculate sea level pressure for operational files

--- I_ForcingFiles/Atmos/convert_files.sh
+++ I_ForcingFiles/Atmos/convert_files.sh
@@ -1,11 +1,11 @@
 #/bin/bash
 
-DIR=/home/dlatorne/MEOPAR/CGRF/NEMO-atmos/slp_y*
-SAV=/home/dlatorne/MEOPAR/CGRF/NEMO-atmos/
+DIR=/ocean/sallen/allen/research/Meopar/Operational/ops_y2012m12*.nc
+SAV=/ocean/nsoontie/MEOPAR/GEM2.5/ops/
 
 for i in $( ls $DIR ); do
     PRE=$i
-    TMP=${i/slp_/t2_}
+    TMP=$i
     
-    python correct_pressure.py $PRE $TMP $SAV
+    python correct_pressure_ops.py $PRE $TMP $SAV
 done
