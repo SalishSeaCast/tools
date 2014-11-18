a3c1b6e409519f1a65a8e59a7da87d3db41f85ca
Doug Latornell <djl@douglatornell.ca>
2014-11-18 15:42:44 -0800

    Add ssh_PtAtkinson() to nowcast.figures module.

--- SalishSeaTools/salishsea_tools/nowcast/figures.py
+++ SalishSeaTools/salishsea_tools/nowcast/figures.py
@@ -19,3 +19,35 @@ figures for analysis and model evaluation of daily nowcast/forecast runs.
 from __future__ import division
 
 import matplotlib.pyplot as plt
+
+from salishsea_tools import nc_tools
+
+
+def ssh_PtAtkinson(grid_T, bathy=None, figsize=(12, 4)):
+    """Return a figure containing a plot of hourly sea surface height at
+    Pt. Atkinson.
+
+    :arg grid_T: Hourly tracer results dataset from NEMO.
+    :type grid_T: :class:`netCDF4.Dataset`
+
+    :arg bathy: Bathymetry dataset for the Salish Sea NEMO model.
+    :type bathy: :class:`netCDF4.Dataset`
+
+    :arg figsize: Figure size (width, height) in inches.
+    :type figsize: 2-tuple
+
+    :returns: Matplotlib figure object
+    """
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+    ssh = grid_T.variables['sossheig']
+    results_date = nc_tools.timestamp(grid_T, 0).format('YYYY-MM-DD')
+    ax.plot(ssh[:, 468, 328], 'o')
+    ax.set_xlim(0, 23)
+    ax.set_xlabel('UTC Hour on {}'.format(results_date))
+    ax.set_ylabel(
+        '{label} [{units}]'
+        .format(label=ssh.long_name.title(), units=ssh.units))
+    ax.grid()
+    ax.set_title(
+        'Pt. Atkinson Hourly Sea Surface Height on {}'.format(results_date))
+    return fig
