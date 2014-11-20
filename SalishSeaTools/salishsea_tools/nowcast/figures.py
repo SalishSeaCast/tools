ab8fc5be9d783cd138687297543a8a06b68c58e0
Idalia <imachuca@eos.ubc.ca>
2014-11-20 13:06:54 -0800

    added PA_tidal_predictions (test)

--- SalishSeaTools/salishsea_tools/nowcast/figures.py
+++ SalishSeaTools/salishsea_tools/nowcast/figures.py
@@ -51,3 +51,47 @@ def ssh_PtAtkinson(grid_T, bathy=None, figsize=(12, 4)):
     ax.set_title(
         'Pt. Atkinson Hourly Sea Surface Height on {}'.format(results_date))
     return fig
+
+
+
+def PA_tidal_predictions(grid_T,figsize=(10,5)):
+    """ Plots the tidal cycle at Point Atkinson during a 4 week period centred around the dsimulation start date.
+    Assumes that a tidal prediction file exists in a specific directory.
+    
+    :arg gridT: Hourly tracer results dataset from NEMO.
+    :type gridT: :class:`netCDF4.Dataset`
+
+    :arg figsize: figure size (width, height) in inches.
+    :type figsize: 2-tuple
+    
+    :returns: Matplotlib figure object instance
+    """
+     #beginning and end time of the simulation file.
+    t_orig=(nc_tools.timestamp(gridT,0)).datetime
+    t_end =((nc_tools.timestamp(gridT,-1)).datetime)
+    
+    #set axis limits 2 weeks before and after start date
+    ax_start = t_orig - datetime.timedelta(weeks=2)
+    ax_end = t_orig + datetime.timedelta(weeks=2)
+    ylims=[-3,3]
+    
+    #load the tidal prediciton file
+    path='/data/nsoontie/MEOPAR/analysis/Susan/'
+    filename='Point Atkinson_t_tide_compare8_31-Dec-{}_02-Jan-{}.csv'.format(t_orig.year-1,t_orig.year+1)
+    tfile = path+filename
+    ttide,msl= stormtools.load_tidal_predictions(tfile)
+    
+    #plotting
+    fig,ax=plt.subplots(1,1,figsize=figsize)
+    fig.autofmt_xdate()
+    ax.plot(ttide.time,ttide.pred_all,'-k')
+    #line indicating current date
+    ax.plot([t_orig,t_orig],ylims,'-r')
+    ax.plot([t_end,t_end],ylims,'-r')
+    #axis limits and labels
+    ax.set_xlim([ax_start,ax_end])
+    ax.set_ylim(ylims)
+    ax.set_title('Tidal Predictions at Point Atkinson')
+    ax.set_ylabel('SSH [m]')
+    
+    return fig
