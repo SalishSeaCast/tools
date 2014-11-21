ab39f462ba380fbb3b619add7694ac5c27d282fc
Idalia <imachuca@eos.ubc.ca>
2014-11-20 16:04:25 -0800

    added PA_max_ssh (working condition)

--- SalishSeaTools/salishsea_tools/nowcast/figures.py
+++ SalishSeaTools/salishsea_tools/nowcast/figures.py
@@ -23,7 +23,7 @@ from __future__ import division
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from salishsea_tools import nc_tools, stormtools, tidetools
+from salishsea_tools import nc_tools, viz_tools, stormtools, tidetools
 import datetime
 import requests
 import arrow
@@ -171,13 +171,13 @@ def get_NOAA_wlevels(station_no, start_date, end_date):
     """ Retrieves recent, 6 minute interval, NOAA water levels relative to mean sea level
     from a station in a given date range.
     
-    :arg station_no: the NOAA station number.
+    :arg station_no: NOAA station number.
     :type station_no: integer
     
-    :arg start_date: the start of the date range eg. 01-Jan-2014.
+    :arg start_date: The start of the date range eg. 01-Jan-2014.
     :type start_date: string
     
-    :arg end_date: the end of the date range ef. 02-Jan-2014.
+    :arg end_date: The end of the date range ef. 02-Jan-2014.
     :type end_date: string
     
     :returns: DataFrame object with time and wlev columns, among others that are irrelevant.
@@ -211,18 +211,18 @@ def get_NOAA_wlevels(station_no, start_date, end_date):
 def compare_water_levels(name, grid_T, gridB, figsize=(20,5) ):
     """ Compares modelled water levels to observed water levels at a NOAA station over one day.
     
-    :arg name: name of the NOAA station (e.g NeahBay, CherryPoint, FridayHarbor).
+    :arg name: Name of the NOAA station (e.g NeahBay, CherryPoint, FridayHarbor).
     :type name: string
     
     :arg grid_T: Hourly tracer results dataset from NEMO.
     :type grid_T: :class:`netCDF4.Dataset`
     
-    :arg gridB: Model bathymetry file.
+    :arg gridB: Bathymetry dataset for the Salish Sea NEMO model.
     :type gridB: :class:`netCDF4.Dataset`
     
-    :arg figsize: figure size (width, height) in inches.
+    :arg figsize:  Figure size (width, height) in inches
     :type figsize: 2-tuple
-    
+
     :returns: Matplotlib figure object instance
     """
     
@@ -254,5 +254,85 @@ def compare_water_levels(name, grid_T, gridB, figsize=(20,5) ):
     ax.legend(loc=0)
     ax.set_title(name)
     ax.set_ylabel('Water levels wrt MSL (m)')
+
+    return fig
+
+#
+def PA_max_ssh(grid_T, gridB, figsize=(15,10)):
+    """Function that plots the water level at every hour throughout the day and identifies the maximum.
+    It also plots the sea surface height throughout the region for the time when the sea surface height
+    was at its maximum at Point Atkinson.
+    
+    :arg grid_T: Hourly tracer results dataset from NEMO.
+    :type grid_T: :class:`netCDF4.Dataset`
+    
+    :arg gridB: Bathymetry dataset for the Salish Sea NEMO model.
+    :type gridB: :class:`netCDF4.Dataset`
+    
+    :arg figsize:  Figure size (width, height) in inches
+    :type figsize: 2-tuple
+    
+    :returns: Matplotlib figure object instance
+    """
+
+    #defining Point Atkinson
+    lon_PA=-123.26
+    lat_PA= 49.33
+    bathy, X, Y = tidetools.get_bathy_data(gridB)
+    [j,i]=tidetools.find_closest_model_point(lon_PA,lat_PA,X,Y,bathy,allow_land=True)
+    
+    #loading sea surface height
+    ssh = grid_T.variables['sossheig']
+    #loading sea surface height at Point Atkinson
+    ssh_loc = ssh[:,j,i]
+    #index when sea surface height is at its maximum at Point Atkinson
+    m = max(ssh_loc)
+    index = [k for k, l in enumerate(ssh_loc) if l == m]
+    index = index[0]
+    #sea surface height when there is a maximum at Point Atkinson
+    ssh_max = np.ma.masked_values(ssh[index], 0)
+    
+    #time for curve
+    count=grid_T.variables['time_counter'][:]
+    t = nc_tools.timestamp(grid_T,np.arange(count.shape[0]))
+    for ind in range(len(t)):
+        t[ind]=t[ind].datetime
+    t_orig=(nc_tools.timestamp(grid_T,0)).datetime
+    start_date = t_orig.strftime('%d-%b-%Y')
+    end_date = (t_orig + datetime.timedelta(days=1)).strftime('%d-%b-%Y')
+    
+    #timestamp
+    timestamp = nc_tools.timestamp(grid_T,index)
+    
+    #figure
+    fig,(ax1,ax2) =plt.subplots(1,2,figsize=figsize) 
+    
+    #curve plot
+    ax1.plot(t,ssh_loc,label='Model')
+    ax1.plot(t[index],ssh_loc[index],color='Indigo',marker='D',markersize=12,label='Maximum SSH')
+    ax1.set_xlim(t_orig,t_orig + datetime.timedelta(days=1))
+    ax1.set_ylim([-3,3])
+    ax1.set_title('Point Atkinson')
+    ax1.set_xlabel(timestamp.strftime('%d-%b-%Y'))
+    ax1.set_ylabel('Water levels wrt MSL (m)')
+    ax1.legend(loc = 0, numpoints = 1)
+    ax1.set_position((0, 0.3, 0.55, 0.4))
+    
+    #ssh profile
+    viz_tools.set_aspect(ax2)
+    land_colour = 'burlywood'
+    ax2.set_axis_bgcolor(land_colour)
+    cmap = plt.get_cmap('RdYlGn_r')
+    cs = [-2.5,-2,-1.5,-1,-0.5,0,0.5,1,1.5,2,2.4,2.8,3]
+    mesh=ax2.contourf(ssh_max,cs,cmap=cmap,extend='both')
+    cbar = fig.colorbar(mesh,ax=ax2)
+    cbar.set_ticks(cs)
+    cbar.set_label('[m]')
+    ax2.grid()
+    ax2.set_xlabel('x Index')
+    ax2.set_ylabel('y Index')
+    viz_tools.plot_coastline(ax2,gridB)
+    ax2.set_title('Sea Suface Height: ' + timestamp.strftime('%d-%b-%Y, %H:%M'))
+    ax2.plot(i,j,marker='D',color='Indigo',ms=8)
 
     return fig
