6cf5cca7a6677a483c9a62c3f5b3f1a465211451
Susan Allen <sallen@eoas.ubc.ca>
2014-11-26 18:03:33 -0800

    Add forecast links to makelinks: no need to split nowcast and forecast links

--- SalishSeaTools/salishsea_tools/nowcast/makelinks.sh
+++ SalishSeaTools/salishsea_tools/nowcast/makelinks.sh
@@ -1,4 +1,4 @@
-# make symbolic links on orcinus
+# make symbolic links on orcinus for nowcast and forecast
 
 # today
 Year=$(date +%Y)
@@ -15,6 +15,11 @@ Yearp1=$(date +%Y -d tomorrow)
 Monthp1=$(date +%m -d tomorrow)
 Dayp1=$(date +%d -d tomorrow)
 
+# Next Day
+Yearp2=$(date +%Y -d "today + 2 day")
+Monthp2=$(date +%m -d "today + 2 day")
+Dayp2=$(date +%d -d "today + 2 day")
+
 cd /home/sallen/MEOPAR/nowcast
 
 # rivers
@@ -24,9 +29,11 @@ ln -s ../../NEMO-forcing/rivers/rivers_month.nc
 riverfilem1=RFraserCElse_y${Yearm1}m${Monthm1}d${Daym1}.nc
 riverfile=RFraserCElse_y${Year}m${Month}d${Day}.nc
 riverfilep1=RFraserCElse_y${Yearp1}m${Monthp1}d${Dayp1}.nc
+riverfilep2=RFraserCElse_y${Yearp2}m${Monthp2}d${Dayp2}.nc
 ln -s ../../rivers/$riverfilem1
 ln -s ../../rivers/$riverfilem1 $riverfile
 ln -s ../../rivers/$riverfilem1 $riverfilep1
+ln -s ../../rivers/$riverfilem1 $riverfilep2
 
 # ssh
 cd ../open_boundaries/west/ssh
@@ -34,9 +41,11 @@ rm -f *
 sshfilem1=ssh_y${Yearm1}m${Monthm1}d${Daym1}.nc
 sshfile=ssh_y${Year}m${Month}d${Day}.nc
 sshfilep1=ssh_y${Yearp1}m${Monthp1}d${Dayp1}.nc
+sshfilep2=ssh_y${Yearp2}m${Monthp2}d${Dayp2}.nc
 ln -s ../../../../sshNeahBay/obs/$sshfilem1
 ln -s ../../../../sshNeahBay/fcst/$sshfile
 ln -s ../../../../sshNeahBay/fcst/$sshfilep1
+ln -s ../../../../sshNeahBay/fcst/$sshfilep2
 
 # weather
 cd ../../../NEMO-atmos
@@ -46,7 +55,9 @@ ln -s ../../NEMO-forcing/grid/weights-gem2.5-ops.nc
 netfilem1=ops_y${Yearm1}m${Monthm1}d${Daym1}.nc
 netfile=ops_y${Year}m${Month}d${Day}.nc
 netfilep1=ops_y${Yearp1}m${Monthp1}d${Dayp1}.nc
+netfilep2=ops_y${Yearp2}m${Monthp2}d${Dayp2}.nc
 ln -s ../../GEM2.5/ops/NEMO-atmos/$netfilem1
 ln -s ../../GEM2.5/ops/NEMO-atmos/$netfile
-ln -s ../../GEM2.5/ops/NEMO-atmos/$netfile $netfilep1
+ln -s ../../GEM2.5/ops/NEMO-atmos/fcst/$netfilep1
+ln -s ../../GEM2.5/ops/NEMO-atmos/fcst/$netfilep2
 
