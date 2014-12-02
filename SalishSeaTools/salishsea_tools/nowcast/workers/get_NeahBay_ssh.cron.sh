75de0753d6e2c95dfe3927399bcbe3d80e299849
Doug Latornell <djl@douglatornell.ca>
2014-12-02 07:29:46 -0800

    Update Python environment path in nowcast worker cron scripts.

--- SalishSeaTools/salishsea_tools/nowcast/workers/get_NeahBay_ssh.cron.sh
+++ SalishSeaTools/salishsea_tools/nowcast/workers/get_NeahBay_ssh.cron.sh
@@ -5,7 +5,7 @@
 #   NOWCAST_TOOLS=tools/SalishSeaTools/salishsea_toola/nowcast
 #   0 10 * * *  ${MEOPAR}/${NOWCAST_TOOLS}/workers/get_NeahBay_ssh.cron.sh
 
-PYTHON=/home/dlatorne/anaconda/bin/python
+PYTHON=/home/dlatorne/anaconda/envs/nowcast/bin/python
 NOWCAST=/home/dlatorne/public_html/MEOPAR/nowcast
 CONFIG=${NOWCAST}/nowcast.yaml
 ${PYTHON} -m salishsea_tools.nowcast.workers.get_NeahBay_ssh ${CONFIG}
--- SalishSeaTools/salishsea_tools/nowcast/workers/weather_06_download.cron.sh
+++ SalishSeaTools/salishsea_tools/nowcast/workers/weather_06_download.cron.sh
@@ -5,7 +5,7 @@
 #   NOWCAST_TOOLS=tools/SalishSeaTools/salishsea_toola/nowcast
 #   0 2 * * *  ${MEOPAR}/${NOWCAST_TOOLS}/workers/weather_06_download.cron.sh
 
-PYTHON=/home/dlatorne/anaconda/bin/python
+PYTHON=/home/dlatorne/anaconda/envs/nowcast/bin/python
 NOWCAST=/home/dlatorne/public_html/MEOPAR/nowcast
 CONFIG=${NOWCAST}/nowcast.yaml
 ${PYTHON} -m salishsea_tools.nowcast.workers.download_weather ${CONFIG} 06
