78ba68f7cb6b9f8a9763b1f9f11e551f26bd673d
Susan Allen <sallen@eos.ubc.ca>
2014-12-03 13:16:07 -0800

    Corrected comments.

--- SalishSeaTools/salishsea_tools/nowcast/workers/weather_00_download.cron.sh
+++ SalishSeaTools/salishsea_tools/nowcast/workers/weather_00_download.cron.sh
@@ -3,7 +3,7 @@
 # usage:
 #   MEOPAR=/data/dlatorne/MEOPAR
 #   NOWCAST_TOOLS=tools/SalishSeaTools/salishsea_toola/nowcast
-#   0 2 * * *  ${MEOPAR}/${NOWCAST_TOOLS}/workers/weather_06_download.cron.sh
+#   0 2 * * *  ${MEOPAR}/${NOWCAST_TOOLS}/workers/weather_00_download.cron.sh
 
 PYTHON=/home/dlatorne/anaconda/envs/nowcast/bin/python
 NOWCAST=/home/dlatorne/public_html/MEOPAR/nowcast
