fd8f2876a26cfc3f61f41077acae57948513fa05
Idalia Machuca <14225094+imachuca24@users.noreply.github.com>
2014-12-08 15:32:42 -0800

    new function: average_winds_at_station

--- SalishSeaTools/salishsea_tools/nowcast/figures.py
+++ SalishSeaTools/salishsea_tools/nowcast/figures.py
@@ -760,6 +760,82 @@ def winds_at_station(station, gridB,gridW,figsize=(15,10)):
         ax.set_title('Daily average winds at ' + name,**title_font)
         
     return fig
+
+def average_winds_at_station(grid_T, gridB, model_path, station,  figsize=(15,10)):
+    """ Figure that plots winds at individual or all stations.
+
+      :arg grid_T: Hourly tracer results dataset from NEMO.
+      :type grid_T: :class:`netCDF4.Dataset`
+      
+      :arg gridB: Bathymetry dataset for the Salish Sea NEMO model.
+      :type gridB: :class:`netCDF4.Dataset`
+      
+      :arg model_path: directory where the model files are stored
+      :type model_path: string
+      
+      :arg station: Either the name of one of the seven defined stations or 'all' for all stations
+      :type station: string
+
+      :arg figsize:  Figure size (width, height) in inches
+      :type figsize: 2-tuple
+
+      :returns: Matplotlib figure object instance
+  
+    """
+        
+    lats={'Point Atkinson': 49.33,'Campbell River': 50.04, 'Victoria': 48.41, 
+          'Cherry Point': 48.866667,'Neah Bay': 48.4, 'Friday Harbor': 48.55,
+          'Sandheads': 49.10}
+    lons={'Point Atkinson': -123.25, 'Campbell River':-125.24, 'Victoria': -123.36, 
+          'Cherry Point': -122.766667, 'Neah Bay':-124.6, 'Friday Harbor': -123.016667,
+          'Sandheads': -123.30}
+    
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+    ax.grid()  
+    viz_tools.set_aspect(ax)
+    viz_tools.plot_land_mask(ax, gridB,color='burlywood',coords='map')
+    viz_tools.plot_coastline(ax,gridB,coords='map')  
+    ax.set_xlabel('longitude',**axis_font)
+    ax.set_ylabel('latitude',**axis_font)
+    scale = 0.1
+    
+    [t_orig,t_final,t] = get_model_time_variables(grid_T)
+      
+    def plot(name, scale):
+       [wind, direc, t, pr, tem, sol, the, qr, pre] = get_model_winds(lons[name],lats[name],t_orig,t_final,model_path)
+       uwind = wind*np.cos(np.radians(direc))
+       vwind=wind*np.sin(np.radians(direc))
+       uaverage = np.mean(uwind, axis=0)
+       vaverage = np.mean(vwind, axis=0)
+       speeds = wind
+        
+       ax.plot(lons[name], lats[name], marker='D', color='MediumOrchid',
+                markersize=10, markeredgewidth=2)
+         #use arrow rather than quiver as we are plotting one at a time
+       ax.arrow(lons[name],  lats[name], 
+                 scale*uaverage, scale*vaverage, 
+                 head_width=0.05, head_length=0.1, width=0.02, 
+                 color='b',fc='b', ec='b',)
+       return ax, uwind, vwind
+
+    ax.arrow(-123, 50., 5.*scale, 0.*scale,
+              head_width=0.05, head_length=0.1, width=0.02, 
+              color='b',fc='b', ec='b')
+    ax.text(-123, 50.1, "5 m/s")
+    if station == 'all':
+        names=['Point Atkinson','Campbell River','Victoria','Cherry Point','Neah Bay','Friday Harbor','Sandheads']
+        m = np.arange(len(names))
+        for name, M in zip (names, m):
+            plot(name, scale)
+            ax.set_title('Daily average winds at all stations',**title_font)
+        
+    
+    if station == 'Point Atkinson' or station == 'Campbell River' or station =='Victoria' or station =='Cherry Point' or station == 'Neah Bay' or station == 'Friday Harbor' or station =='Sandheads':
+        name = station
+        plot(name,scale)
+        ax.set_title('Daily average winds at ' + name,**title_font)
+        
+    return fig
     
 def thalweg_salinity(grid_T_d,figsize=(20,8),cs = [26,27,28,29,30,30.2,30.4,30.6,30.8,31,32,33,34]):
     """ Plot the daily averaged salinity field along the thalweg.
