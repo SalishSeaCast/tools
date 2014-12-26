0aad4436b0a499508935e5b1734e35468ffdbeff
Susan Allen <sallen@eos.ubc.ca>
2014-12-26 13:59:09 -0800

    Duplicate the file before building the html site.

--- SalishSeaTools/salishsea_tools/nowcast/plots_up_part2.sh
+++ SalishSeaTools/salishsea_tools/nowcast/plots_up_part2.sh
@@ -1,7 +1,18 @@
+# copy the mako file over to www_working.  make a date named copy.  make html 
+# do the rst
+
+# today
+Year=$(date +%Y)
+Month=$(date +%b)
+Month=$(echo $Month | tr '[:upper:]' '[:lower:]')
+Day=$(date +%d)
+
 cd /ocean/sallen/allen/research/MEOPAR/www_working/salishsea-site/site/storm-surge
 oridir=/ocean/sallen/allen/research/MEOPAR/Tools/SalishSeaTools/salishsea_tools/nowcast/www/templates
 cp $oridir/forecast.mako forecast.rst
+cp forecast.rst forecast_${Day}${Month}${Year}.rst
+
 cd ..
 make html
-rsync -tvhz _build/html/storm-surge/forecast.html ~/public_html/MEOPAR/site/storm-surge/
+rsync -tvhz _build/html/storm-surge/ ~/public_html/MEOPAR/site/storm-surge/
 rsync -rtvhz _build/html/_static/nemo/results_figures/ ~/public_html/MEOPAR/site/_static/nemo/results_figures/
