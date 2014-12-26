992b1d19573cc9ae84b64cf21819983ce36359da
Susan Allen <sallen@eoas.ubc.ca>
2014-12-26 14:36:10 -0800

    Various edits to correct bugs and make the scripts actually work.

--- SalishSeaTools/salishsea_tools/nowcast/plots_up_part2.sh
+++ SalishSeaTools/salishsea_tools/nowcast/plots_up_part2.sh
@@ -1,11 +1,11 @@
 # copy the mako file over to www_working.  make a date named copy.  make html 
 # do the rst
 
-# today
-Year=$(date +%Y)
-Month=$(date +%b)
+# tomorrow
+Year=$(date +%y -d tomorrow)
+Month=$(date +%b -d tomorrow)
 Month=$(echo $Month | tr '[:upper:]' '[:lower:]')
-Day=$(date +%d)
+Day=$(date +%d -d tomorrow)
 
 cd /ocean/sallen/allen/research/MEOPAR/www_working/salishsea-site/site/storm-surge
 oridir=/ocean/sallen/allen/research/MEOPAR/Tools/SalishSeaTools/salishsea_tools/nowcast/www/templates
@@ -14,5 +14,5 @@ cp forecast.rst forecast_${Day}${Month}${Year}.rst
 
 cd ..
 make html
-rsync -tvhz _build/html/storm-surge/ ~/public_html/MEOPAR/site/storm-surge/
+rsync -rtvhz _build/html/storm-surge/ ~/public_html/MEOPAR/site/storm-surge/
 rsync -rtvhz _build/html/_static/nemo/results_figures/ ~/public_html/MEOPAR/site/_static/nemo/results_figures/
