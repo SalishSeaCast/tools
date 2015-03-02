71c6d3456982f42e94f67002f6864293a8d20fa3
Idalia <imachuca@eos.ubc.ca>
2015-03-02 12:45:47 -0800

    function docstrings (except final forcing plot)

--- SalishSeaTools/salishsea_tools/nowcast/analyze.py
+++ SalishSeaTools/salishsea_tools/nowcast/analyze.py
@@ -58,6 +58,30 @@ MSL_DATUMS = {
     'Campbell River': 2.89, 'Patricia Bay': 2.30}
 
 
+def model_paths():
+    """ A dictionary of paths containing model results for
+    nowcasts, forecasts and forecasts 2.
+
+    :returns: paths (dictionary)
+    """
+
+    paths = {'nowcast': '/data/dlatorne/MEOPAR/SalishSea/nowcast/',
+             'forecast': '/ocean/sallen/allen/research/MEOPAR/SalishSea/forecast/',
+             'forecast2': '/ocean/sallen/allen/research/MEOPAR/SalishSea/forecast2/'}
+
+    return paths
+
+
+def feet_to_metres(feet):
+    """ Converts feet to metres.
+
+    :returns: metres
+    """
+
+    metres = feet*0.3048
+    return metres
+
+
 def get_filenames(t_orig, t_final, period, grid, model_path):
     """Returns a list with the filenames for all files over the
     defined period of time and sorted in chronological order.
@@ -258,3 +282,272 @@ def compare_ssh_tides(grid_B, files, t_orig, t_final, name, PST=0, MSL=0):
     ax.legend(loc=3, ncol=2)
 
     return fig
+
+
+def calculate_wlev_residual(name, t_orig):
+    """ Calculates the residual of the observed water levels with respect
+    to the predicted tides at a specific station and for a specific date.
+
+    :arg name: Name of station.
+    :type name: string
+
+    :arg t_orig: The beginning of the date range of interest.
+    :type t_orig: datetime object
+
+    :returns: residual (calculated residual), obs (observed water levels),
+              tides (predicted tides)
+    """
+    stations = {'Cherry Point': 9449424,
+                'Neah Bay': 9443090,
+                'Friday Harbor': 9449880}
+    start_date = t_orig.strftime('%d-%b-%Y')
+    end_date = start_date
+    obs = figures.get_NOAA_wlevels(stations[name], start_date, end_date)
+    tides = figures.get_NOAA_tides(stations[name], start_date, end_date)
+
+    # Prepare to find residual
+    residual = np.zeros(len(obs.time))
+
+    # Residual and time check
+    for i in np.arange(0, len(obs.time)):
+        if any(tides.time == obs.time[i]):
+            residual[i] = obs.wlev[i] - tides.pred[tides.time == obs.time[i]]
+        else:
+            residual[i] = float('Nan')
+
+    return residual, obs, tides
+
+
+def plot_wlev_residual(t_orig, elements, figsize=(20, 5)):
+    """ Plots the water level residual as calculated by the function
+    calculate_wlev_residual and has the option to also plot the observed
+    water levels and predicted tides over the course of one day.
+
+    :arg t_orig: The beginning of the date range of interest.
+    :type t_orig: datetime object
+
+    :arg elements: Elements included in figure.
+                   'residual' for residual only and 'all' for residual,
+                   observed water level, and predicted tides.
+    :type elements: string
+
+    :arg figsize: Figure size (width, height) in inches.
+    :type figsize: 2-tuple
+
+    :returns: fig
+    """
+
+    residual, obs, tides = calculate_wlev_residual('Neah Bay', t_orig)
+
+    # Figure
+    fig, ax = plt.subplots(1, 1, figsize=figsize)
+
+    # Plot
+    ax.plot(obs.time, residual, 'Gray', label='Obs Residual', linewidth=2)
+    if elements == 'all':
+        ax.plot(obs.time, obs.wlev,
+                'DodgerBlue', label='Obs Water Level', linewidth=2)
+        ax.plot(tides.time, tides.pred[tides.time == obs.time],
+                'ForestGreen', label='Pred Tides', linewidth=2)
+    if elements == 'residual':
+        pass
+    ax.legend()
+    hfmt = mdates.DateFormatter('%m/%d %H:%M')
+    ax.xaxis.set_major_formatter(hfmt)
+    fig.autofmt_xdate()
+
+    return fig
+
+
+def create_path_sshNB(mode, t_orig):
+    """ Creates a complete path to the .txt file containing
+    predicted water levels at Neah Bay for the specified date
+    and for the mode chosen.
+
+    :arg mode: Mode of results - nowcast, forecast, forecast2.
+    :type mode: string
+
+    :arg t_orig: The beginning of the date range of interest.
+    :type t_orig: datetime object
+
+    :returns: filename_NB (path including filename)
+              and run_date, a datetime object of the simulation run date
+    """
+
+    # All paths
+    paths = model_paths()
+
+    run_date = t_orig
+
+    # Directory housing all daily nowcast or forecast folders
+    if mode == 'nowcast':
+        results_home = paths['nowcast']
+    elif mode == 'forecast':
+        results_home = paths['forecast']
+        run_date = run_date + datetime.timedelta(days=-1)
+    elif mode == 'forecast2':
+        results_home = paths['forecast2']
+        run_date = run_date + datetime.timedelta(days=-2)
+
+    # Directory housing all result files for one day
+    results_dir = os.path.join(results_home,
+                               run_date.strftime('%d%b%y').lower())
+
+    # Directory and filename
+    filename_NB = glob.glob(results_dir+'/ssh*')
+    filename_NB = filename_NB[0]
+
+    return filename_NB, run_date
+
+
+def create_path_results(mode, t_orig):
+    """ Compiles a list of complete paths to the various
+    model result .nc files (both 1d and 1h and grid U, V, W and T)
+    for the specified mode and date.
+
+    :arg mode: Mode of results - nowcast, forecast, forecast2.
+    :type mode: string
+
+    :arg t_orig: The beginning of the date range of interest.
+    :type t_orig: datetime object
+
+    :returns: filenames (list of paths including filenames)
+    """
+
+    # All paths
+    paths = model_paths()
+
+    run_date = t_orig
+
+    # Directory housing all daily nowcast or forecast folders
+    if mode == 'nowcast':
+        results_home = paths['nowcast']
+    elif mode == 'forecast':
+        results_home = paths['forecast']
+        run_date = run_date + datetime.timedelta(days=-1)
+    elif mode == 'forecast2':
+        results_home = paths['forecast2']
+        run_date = run_date + datetime.timedelta(days=-2)
+
+    results_dir = os.path.join(results_home,
+                               run_date.strftime('%d%b%y').lower())
+    filenames = glob.glob(results_dir+'/SalishSea_*_grid_*.nc')
+
+    return filenames
+
+
+def verified_runs(t_orig):
+    """ Compiles a list of run types (nowcast, forecast, and/or forecast 2)
+    that have been verified as complete by checking if their corresponding
+    .nc files for that day (generated by create_path_results) exist.
+
+    :arg t_orig: The beginning of the date range of interest.
+    :type t_orig: datetime object
+
+    :returns: runs_list (list of strings)
+    """
+
+    runs_list = []
+    for mode in ['nowcast', 'forecast', 'forecast2']:
+        files = create_path_results(mode, t_orig)
+        if files:
+            runs_list.append(mode)
+
+    return runs_list
+
+
+def load_surge_data(filename_NB):
+    """Loads the textfile with surge predictions for Neah Bay.
+
+    :arg filename_NB: Path to file of predicted water levels at Neah Bay.
+    :type filename_NB: string
+
+    :returns: data (data structure)
+    """
+
+    # Loading the data from that text file.
+    data = pd.read_csv(filename_NB, skiprows=3,
+                       names=['date', 'surge', 'tide', 'obs',
+                              'fcst', 'anom', 'comment'], comment='#')
+    # Drop rows with all Nans
+    data = data.dropna(how='all')
+
+    return data
+
+
+def to_datetime(datestr, year, isDec, isJan):
+    """ Converts the string given by datestr to a datetime object.
+    The year is an argument because the datestr in the NOAA data
+    doesn't have a year. Times are in UTC/GMT.
+
+    :arg datestr: Date of data.
+    :type datestr: datetime object
+
+    :arg year: Year of data.
+    :type year: datetime object
+
+    :arg isDec: True if run date was December.
+    :type isDec: Boolean
+
+    :arg isJan: True if run date was January.
+    :type isJan: Boolean
+
+    :returns: dt (datetime representation of datestr)
+    """
+
+    dt = datetime.datetime.strptime(datestr, '%m/%d %HZ')
+    # Dealing with year changes.
+    if isDec and dt.month == 1:
+        dt = dt.replace(year=year+1)
+    elif isJan and dt.month == 12:
+        dt = dt.replace(year=year-1)
+    else:
+        dt = dt.replace(year=year)
+    dt = dt.replace(tzinfo=tz.tzutc())
+
+    return dt
+
+
+def retrieve_surge(data, run_date):
+    """ Gathers the surge information a forcing file from on run_date.
+
+    :arg data: Surge predictions data.
+    :type data: data structure
+
+    :arg run_date: Simulation run date.
+    :type run_date: datetime object
+
+    :returns: surges (meteres), times (array with time_counter)
+    """
+
+    surge = []
+    times = []
+    isDec, isJan = False, False
+    if run_date.month == 1:
+        isJan = True
+    if run_date.month == 12:
+        isDec = True
+    # Convert datetime to string for comparing with times in data
+    for d in data.date:
+
+        dt = to_datetime(d, run_date.year, isDec, isJan)
+        times.append(dt)
+        daystr = dt.strftime('%m/%d %HZ')
+        tide = data.tide[data.date == daystr].item()
+        obs = data.obs[data.date == daystr].item()
+        fcst = data.fcst[data.date == daystr].item()
+        if obs == 99.90:
+            # Fall daylight savings
+            if fcst == 99.90:
+                # If surge is empty, just append 0
+                if not surge:
+                    surge.append(0)
+                else:
+                    # Otherwise append previous value
+                    surge.append(surge[-1])
+            else:
+                surge.append(feet_to_metres(fcst-tide))
+        else:
+            surge.append(feet_to_metres(obs-tide))
+
+    return surge, times
