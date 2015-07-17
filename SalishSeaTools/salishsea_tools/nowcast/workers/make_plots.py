168ceec06821fddce2719b00e4d459dc9bd76261
Nancy Soontiens <nsoontie@eos.ubc.ca>
2015-07-17 12:46:56 -0700

    Added thalweg temperature plot to make_plots and make_site_page

--- SalishSeaTools/salishsea_tools/nowcast/workers/make_plots.py
+++ SalishSeaTools/salishsea_tools/nowcast/workers/make_plots.py
@@ -282,6 +282,11 @@ def make_research_plots(
         plots_dir, 'Salinity_on_thalweg_{date}.svg'.format(date=dmy))
     fig.savefig(filename, facecolor=fig.get_facecolor(), bbox_inches='tight')
 
+    fig = figures.thalweg_temperature(grid_T_dy)
+    filename = os.path.join(
+        plots_dir, 'Temperature_on_thalweg_{date}.svg'.format(date=dmy))
+    fig.savefig(filename, facecolor=fig.get_facecolor(), bbox_inches='tight')
+
     fig = figures.plot_surface(grid_T_dy, grid_U_dy, grid_V_dy, bathy)
     filename = os.path.join(
         plots_dir, 'T_S_Currents_on_surface_{date}.svg'.format(date=dmy))
