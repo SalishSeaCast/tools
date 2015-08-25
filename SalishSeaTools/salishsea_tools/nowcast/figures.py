7491f9a985dc0f0ca9eb536460daf739be53e411
alex <h_alex2000@hotmail.com>
2015-08-25 14:40:40 -0700

    modified temperature contour on thalway plot

--- SalishSeaTools/salishsea_tools/nowcast/figures.py
+++ SalishSeaTools/salishsea_tools/nowcast/figures.py
@@ -2012,7 +2012,7 @@ def thalweg_salinity(
 
 def thalweg_temperature(
     grid_T_d, figsize=(20, 8),
-    cs = [6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+    cs = [6.9, 7,7.5, 8, 8.5, 9, 9.8,9.9,10.3,10.5, 11,11.5, 12, 13, 14, 15, 16, 17, 18, 19],
 ):
     """Plots the daily average temperature field along the thalweg.
 
