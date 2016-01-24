effdae600a6f9c4e8c65db32853ec912e8069e15
Doug Latornell <djl@douglatornell.ca>
2016-01-24 12:22:10 -0800

    Add wind_tools.calc_wind_avg_at_point() function.
    Present implementation satisfies the needs of nowcast.figures and
    nowcast.workers.make_feeds modules.

--- SalishSeaTools/salishsea_tools/wind_tools.py
+++ SalishSeaTools/salishsea_tools/wind_tools.py
@@ -19,8 +19,12 @@ data.
 from __future__ import division
 
 from collections import namedtuple
+from pathlib import Path
 
+import netCDF4 as nc
 import numpy as np
+
+from salishsea_tools import nc_tools
 
 # For convenience we import the wind speed conversion factors
 # and functions and wind direction manipulation functions so that they
@@ -37,6 +41,7 @@ from salishsea_tools.unit_conversions import (
 
 
 __all__ = [
+    'calc_wind_avg_at_point',
     'M_PER_S__KM_PER_HR', 'M_PER_S__KNOTS', 'mps_kph', 'mps_knots',
     'wind_to_from', 'bearing_heading',
     'wind_speed_dir',
@@ -65,3 +70,62 @@ def wind_speed_dir(u_wind, v_wind):
     dir = np.rad2deg(dir + (dir < 0) * 2 * np.pi)
     speed_dir = namedtuple('speed_dir', 'speed, dir')
     return speed_dir(speed, dir)
+
+
+def calc_wind_avg_at_point(date_time, weather_path, windji, avg_hrs=-4):
+    """Calculate the average wind components for a time period at
+    a single weather dataset grid point.
+
+    .. note::
+
+        The present implementation only supports averaging over a time
+        period ending at:kbd:`date_time` and values for :kbd:`avg_hrs`
+        in the range :kbd:`[-24, 0]`.
+
+    :arg date_time: Date and time to use as base for the calculation.
+    :type date_time: :py:class:`arrow.arrow.Arrow`
+
+    :arg str weather_path: The directory where weather dataset files
+                           are stored.
+
+    :arg 2-tuple windji: Indices of weather datasset grid point to
+                         calculate the average at as
+                         :kbd:`(lon_index, lat_index)`.
+
+    :arg float avg_hrs: Number of hours to calculate
+
+    :returns: 2-tuple containing the averaged wind components.
+              The :py:attr:`u` attribute holds the u-direction component,
+              and the :py:attr:`v` attribute holds the v-direction
+              component.
+    :rtype: :py:class:`collections.namedtuple`
+
+    :raises: :py:exc:`IndexError` if :kbd:`avg_hrs` is outside the range
+             :kbd:`[-24, 0]`.
+    """
+    weather_filename_tmpl = 'ops_y{0.year:4d}m{0.month:02d}d{0.day:02d}.nc'
+    try:
+        weather_file = Path(
+            weather_path, weather_filename_tmpl.format(date_time))
+        grid_weather = nc_tools.dataset_from_path(weather_file)
+    except IOError:
+        weather_file = Path(
+            weather_path, 'fcst', weather_filename_tmpl.format(date_time))
+        grid_weather = nc_tools.dataset_from_path(weather_file)
+    wind_u, wind_v, wind_t = nc_tools.uv_wind_timeseries_at_point(
+        grid_weather, *windji)
+    if date_time.hour < abs(avg_hrs):
+        grid_weather = nc_tools.dataset_from_path(
+            weather_file.with_name(
+                weather_filename_tmpl.format(date_time.replace(days=-1))))
+        wind_prev_day = nc_tools.uv_wind_timeseries_at_point(
+            grid_weather, *windji)
+        wind_u = np.concatenate((wind_prev_day.u, wind_u))
+        wind_v = np.concatenate((wind_prev_day.v, wind_v))
+        wind_t = np.concatenate((wind_prev_day.time, wind_t))
+    i_date_time = np.asscalar(
+        np.where(wind_t == date_time.floor('hour'))[0])
+    u_avg = np.mean(wind_u[(i_date_time + avg_hrs):i_date_time])
+    v_avg = np.mean(wind_v[(i_date_time + avg_hrs):i_date_time])
+    wind_avg = namedtuple('wind_avg', 'u, v')
+    return wind_avg(u_avg, v_avg)
