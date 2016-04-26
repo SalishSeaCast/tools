b30876c5235b437a927bf53fa4a2c496b0c06d11
Doug Latornell <djl@douglatornell.ca>
2016-04-26 10:21:32 -0700

    Move haversine() from tidetools to geo_tools module.

--- SalishSeaTools/salishsea_tools/geo_tools.py
+++ SalishSeaTools/salishsea_tools/geo_tools.py
@@ -15,9 +15,15 @@
 
 """Functions for working with geographical data and model results.
 """
-import numpy as np
+from math import (
+    asin,
+    cos,
+    radians,
+    sin,
+    sqrt,
+)
 
-from salishsea_tools import tidetools
+import numpy as np
 
 
 def distance_along_curve(lons, lats):
@@ -34,8 +40,34 @@ def distance_along_curve(lons, lats):
     """
     dist = [0]
     for i in np.arange(1, lons.shape[0]):
-        newdist = dist[i-1] + tidetools.haversine(lons[i], lats[i],
+        newdist = dist[i-1] + haversine(lons[i], lats[i],
                                                   lons[i-1], lats[i-1])
         dist.append(newdist)
     dist = np.array(dist)
     return dist
+
+
+def haversine(lon1, lat1, lon2, lat2):
+    """Calculate the great-circle distance in kilometers between two points
+    on a sphere from their longitudes and latitudes.
+
+    Reference: http://www.movable-type.co.uk/scripts/latlong.html
+
+    :arg float lon1: Longitude of point 1.
+
+    :arg float lat1: Latitude of point 1.
+
+    :arg float lon2: Longitude of point 2.
+
+    :arg float lat2: Latitude of point 2.
+
+    :returns: Great-circle distance between two points in km
+    :rtype: float
+    """
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    km = 6367 * c
+    return km
