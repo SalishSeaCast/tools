9b36a892609fd33bea8f5a269b0d7d0c31cc512c
Doug Latornell <djl@douglatornell.ca>
2016-05-09 15:36:50 -0700

    Update make_readme.py script and generated README.md file.

--- bathymetry/make_readme.py
+++ bathymetry/make_readme.py
@@ -1,67 +1,49 @@
-"""Salish Sea NEMO IPython Notebook collection README generator
+# Copyright 2013-2016 The Salish Sea MEOPAR contributors
+# and The University of British Columbia
 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 
-Copyright 2013-2016 The Salish Sea MEOPAR Contributors
-and The University of British Columbia
+#    http://www.apache.org/licenses/LICENSE-2.0
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-   http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+"""Salish Sea NEMO Jupyter Notebook collection README generator
 """
 import datetime
+import glob
 import json
 import os
 import re
 
 
-nbviewer = 'http://nbviewer.ipython.org/urls'
-repo = 'bitbucket.org/salishsea/tools/raw/tip'
-repo_dir = 'bathymetry'
-url = os.path.join(nbviewer, repo, repo_dir)
-title_pattern = re.compile('#{1,6} ?')
-readme = """The IPython Notebooks in this directory are for manipulating
+NBVIEWER = 'http://nbviewer.jupyter.org/urls'
+REPO = 'bitbucket.org/salishsea/tools/raw/tip'
+REPO_DIR = 'bathymetry'
+TITLE_PATTERN = re.compile('#{1,6} ?')
+
+
+def main():
+    url = os.path.join(NBVIEWER, REPO, REPO_DIR)
+    readme = """\
+The IPython Notebooks in this directory are for manipulating
 and visualizing bathymetry netCDF files.
 
 The links below are to static renderings of the notebooks via
-[nbviewer.ipython.org](http://nbviewer.ipython.org/).
-Descriptions below the links are from the first cell of the notebooks
+[nbviewer.jupyter.org](http://nbviewer.jupyter.org/).
+Descriptions under the links below are from the first cell of the notebooks
 (if that cell contains Markdown or raw text).
 
 """
-notebooks = (fn for fn in os.listdir('./') if fn.endswith('ipynb'))
-for fn in notebooks:
-    readme += '* ##[{fn}]({url}/{fn})  \n    \n'.format(fn=fn, url=url)
-    with open(fn, 'rt') as notebook:
-        contents = json.load(notebook)
-    try:
-        first_cell = contents['worksheets'][0]['cells'][0]
-    except KeyError:
-        first_cell = contents['cells'][0]
-    first_cell_type = first_cell['cell_type']
-    if first_cell_type in 'markdown raw'.split():
-        desc_lines = first_cell['source']
-        for line in desc_lines:
-            suffix = ''
-            if title_pattern.match(line):
-                line = title_pattern.sub('**', line)
-                suffix = '**'
-            if line.endswith('\n'):
-                readme += (
-                    '    {line}{suffix}  \n'
-                    .format(line=line[:-1], suffix=suffix))
-            else:
-                readme += (
-                    '    {line}{suffix}  '.format(line=line, suffix=suffix))
-        readme += '\n' * 2
-license = """
+    for fn in glob.glob('*.ipynb'):
+        readme += '* ##[{fn}]({url}/{fn})  \n    \n'.format(fn=fn, url=url)
+        readme += notebook_description(fn)
+    license = """
 ##License
 
 These notebooks and files are copyright 2013-{this_year}
@@ -72,6 +54,38 @@ They are licensed under the Apache License, Version 2.0.
 http://www.apache.org/licenses/LICENSE-2.0
 Please see the LICENSE file for details of the license.
 """.format(this_year=datetime.date.today().year)
-with open('README.md', 'wt') as f:
-    f.writelines(readme)
-    f.writelines(license)
+    with open('README.md', 'wt') as f:
+        f.writelines(readme)
+        f.writelines(license)
+
+
+def notebook_description(fn):
+    description = ''
+    with open(fn, 'rt') as notebook:
+        contents = json.load(notebook)
+    try:
+        first_cell = contents['worksheets'][0]['cells'][0]
+    except KeyError:
+        first_cell = contents['cells'][0]
+    first_cell_type = first_cell['cell_type']
+    if first_cell_type not in 'markdown raw'.split():
+        return description
+    desc_lines = first_cell['source']
+    for line in desc_lines:
+        suffix = ''
+        if TITLE_PATTERN.match(line):
+            line = TITLE_PATTERN.sub('**', line)
+            suffix = '**'
+        if line.endswith('\n'):
+            description += (
+                '    {line}{suffix}  \n'
+                .format(line=line[:-1], suffix=suffix))
+        else:
+            description += (
+                '    {line}{suffix}  '.format(line=line, suffix=suffix))
+    description += '\n' * 2
+    return description
+
+
+if __name__ == '__main__':
+    main()
