efdcbda0139301fd54f2ce52541416342659cd02
Doug Latornell <djl@douglatornell.ca>
2016-05-17 11:14:00 -0700

    PEP8 and docstring gardening.

--- SalishSeaTools/salishsea_tools/data_tools.py
+++ SalishSeaTools/salishsea_tools/data_tools.py
@@ -15,54 +15,52 @@
 
 """Functions for loading and processing observational data
 """
-
-import numpy             as np
-import datetime          as dtm
-import dateutil.parser   as dparser
-import scipy.io          as sio
+import numpy as np
+import datetime as dtm
+import dateutil.parser as dparser
+import scipy.io as sio
 
 
 def load_ADCP(daterange, station='central'):
     """Returns the ONC ADCP velocity profiles at a given station
     over a specified daterange.
     
-    :arg daterange: start and end datetimes for the requested data range.
-        (ex. ['yyyy mmm dd', 'yyyy mmm dd'])
-    :type daterange: list or tuple of str
+    :arg sequence daterange: Start and end datetimes for the requested data
+                             range
+                             (e.g. ['yyyy mmm dd', 'yyyy mmm dd']).
 
-    :arg station: requested profile location ('central', 'east', or 'ddl')
-    :type station: str
-    
-    :returns: arrays of :py:class:`Datetime` and depth and masked arrays of
+    :arg str station: Requested profile location ('central', 'east', or 'ddl')
+
+    :returns: Arrays of :py:class:`Datetime` and depth and masked arrays of
         zonal and meridional velocity
     :rtype: :py:class:`Numpy.array` and :py:class:`Numpy.ma.MaskedArray`
     """
-    
+
     # Parse datein
     startdate = dparser.parse(daterange[0])
-    enddate   = dparser.parse(daterange[1])
+    enddate = dparser.parse(daterange[1])
 
     # Load ONC matfile
     grid = sio.loadmat(
         '/ocean/dlatorne/MEOPAR/ONC_ADCP/ADCP{}.mat'.format(station))
-    
+
     # Generate datetime array
     mtimes = grid['mtime'][0]
     datetimes = np.array([dtm.datetime.fromordinal(int(mtime)) +
-                          dtm.timedelta(days = mtime%1) -
-                          dtm.timedelta(days = 366) for mtime in mtimes])
-    
+                          dtm.timedelta(days=mtime % 1) -
+                          dtm.timedelta(days=366) for mtime in mtimes])
+
     # Find daterange indices
     indexstart = abs(datetimes - startdate).argmin()
-    indexend   = abs(datetimes -   enddate).argmin()
-    
+    indexend = abs(datetimes - enddate).argmin()
+
     # Extract time, depth, and velocity vectors
-    datetime = datetimes[indexstart:indexend+1]
-    u0 = grid['utrue'][:, indexstart:indexend+1]/100 # to m/s
-    v0 = grid['vtrue'][:, indexstart:indexend+1]/100 # to m/s
+    datetime = datetimes[indexstart:indexend + 1]
+    u0 = grid['utrue'][:, indexstart:indexend + 1] / 100  # to m/s
+    v0 = grid['vtrue'][:, indexstart:indexend + 1] / 100  # to m/s
     depth = grid['chartdepth'][0]
-    
+
     u = np.ma.masked_invalid(u0)
     v = np.ma.masked_invalid(v0)
-    
+
     return datetime, depth, u, v
