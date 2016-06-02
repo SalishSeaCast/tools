bdef84dfe6e9453975e9445adce4502efacf9a36
James Petrie <jpetrie@eos.ubc.ca>
2016-06-02 16:08:38 -0700

    refactored find_closest_model_point and added more unit tests

--- SalishSeaTools/salishsea_tools/geo_tools.py
+++ SalishSeaTools/salishsea_tools/geo_tools.py
@@ -64,32 +64,86 @@ def haversine(lon1, lat1, lon2, lat2):
     return km
 
 
+def _spiral_search_for_closest_water_point(
+    j, i, land_mask, lon, lat, model_lons, model_lats
+):
+    # Searches in a spiral pattern around grid element (j,i)
+    # for the closest water point to the the coordinate (lat,lon)
+
+    jmax, imax = land_mask.shape
+    # Limit on size of grid search
+    max_search_dist = max(50, int(model_lats.shape[1]/4))
+    closest_point = None
+    j_s, i_s = j, i  # starting point is j, i
+    dj, di = 0, -1
+    # move j_s, i_s in a square spiral centred at j, i
+    while (i_s-i) <= max_search_dist:
+        if any([(j_s-j) == (i_s-i),
+               ((j_s-j) < 0 and (j_s-j) == -(i_s-i)),
+               ((j_s-j) > 0 and (j_s-j) == 1-(i_s-i))]):
+            # Hit the corner of the spiral- change direction
+            dj, di = -di, dj
+        i_s, j_s = i_s+di, j_s+dj  # Take a step to next square
+        if (i_s >= 0
+            and i_s < imax
+            and j_s >= 0
+            and j_s < jmax
+            and not land_mask[j_s, i_s]
+        ):
+            # Found a water point, how close is it?
+            actual_dist = haversine(
+                lon, lat, model_lons[j_s, i_s], model_lats[j_s, i_s])
+            if closest_point is None:
+                min_dist = actual_dist
+                closest_point = (j_s, i_s)
+            elif actual_dist < min_dist:
+                # Keep record of closest point
+                min_dist = actual_dist
+                closest_point = (j_s, i_s)
+            # Assumes grids are square- reduces search radius to only
+            # check grids that could potentially be closer than this
+            grid_dist = int(((i_s-i)**2 + (j_s-j)**2)**0.5)
+            if (grid_dist + 1) < max_search_dist:
+                # Reduce stopping distance for spiral-
+                # just need to check that no points closer than this one
+                max_search_dist = grid_dist + 1
+    if closest_point is not None:
+        return closest_point
+    else:
+        raise ValueError('lat/lon on land and no nearby water point found')
+
+
 def find_closest_model_point(
     lon, lat, model_lons, model_lats, grid='NEMO', land_mask=None,
     tols={
         'NEMO': {'tol_lon': 0.0104, 'tol_lat': 0.00388},
         'GEM2.5': {'tol_lon': 0.016, 'tol_lat': 0.011},
-        }):
+        }
+):
     """Returns the grid coordinates of the closest model point
     to a specified lon/lat. If land_mask is provided, returns the closest
     water point.
 
-    e.g.
-    j, i = find_closest_model_point(-125.5,49.2,model_lons,model_lats,land_mask=bathy.mask)
-    where bathy, model_lons and model_lats are returned from get_bathy_data(grid).
+    Example:
+
+    .. code-block:: python
+    j, i = find_closest_model_point(
+        -125.5,49.2,model_lons,model_lats,land_mask=bathy.mask)
+
+    where bathy, model_lons and model_lats are returned from
+    :py:func:`salishsea_tools.tidetools.get_bathy_data`.
+
     j is the y-index(latitude), i is the x-index(longitude)
 
-    :arg lon: specified longitude
-    :type lon: float
+    :arg float lon: longitude to find closest grid point to
 
-    :arg lat: specified latitude
-    :type lat: float
+    :arg float lat: latitude to find closest grid point to
 
     :arg model_lons: specified model longitude grid
-    :type model_lons: numpy array
+    :type model_lons: :py:obj:`numpy.ndarray`
 
     :arg model_lats: specified model latitude grid
-    :type model_lats: numpy array
+    :type model_lats: :py:obj:`numpy.ndarray`
 
     :arg grid: specify which default lon/lat tolerances
     :type grid: string
@@ -103,68 +157,43 @@ def find_closest_model_point(
     :returns: yind, xind
     """
 
-    # Search for a grid point with longitude or latitude within
+    if grid not in tols:
+        raise KeyError('The provided grid type is not in tols.\
+                        Use another grid type or add your grid type to tols.')
+
+    # Search for a grid point with longitude and latitude within
     # tolerance of measured location
     j_list, i_list = np.where(
         np.logical_and(
-            (np.logical_and(model_lons > lon - tols[grid]['tol_lon'], model_lons < lon + tols[grid]['tol_lon'])),
-            (np.logical_and(model_lats > lat - tols[grid]['tol_lat'], model_lats < lat + tols[grid]['tol_lat']))))
-    if j_list.size > 1 or i_list.size > 1:
-        # several points within tol, calculate distance for all of them and pick closest
-        min_dist = float('Inf')
-        for n in range(len(i_list)):
-            dist = haversine(lon, lat, model_lons[j_list[n], i_list[n]], model_lats[j_list[n], i_list[n]])
-            if dist < min_dist:
-                closest_point = (j_list[n], i_list[n])
-                min_dist = dist
-        j, i = closest_point
-    elif not j_list or not i_list:
+            (np.logical_and(model_lons > lon - tols[grid]['tol_lon'],
+                            model_lons < lon + tols[grid]['tol_lon'])),
+            (np.logical_and(model_lats > lat - tols[grid]['tol_lat'],
+                            model_lats < lat + tols[grid]['tol_lat']))
+        )
+    )
+
+    if len(j_list) == 0:
         raise ValueError(
             'No model point found. tol_lon/tol_lat too small or '
-            'lon/lat outside of domain.'
-        )
-    else:
-        i = i_list
-        j = j_list
+            'lon/lat outside of domain.')
+    try:
+        j, i = map(np.asscalar, (j_list, i_list))
+    except ValueError:
+        # Several points within tolerance
+        # Calculate distances for all and choose the closest
+        dists = haversine(
+            np.array([lon] * i_list.size), np.array([lat] * j_list.size),
+            model_lons[j_list, i_list], model_lats[j_list, i_list])
+        n = dists.argmin()
+        j, i = map(np.asscalar, (j_list[n], i_list[n]))
 
-    # If point is on land and land mask is provided, try to find closest water point
-    if land_mask is not None and land_mask[j, i]:
-        jmax, imax = land_mask.shape
-        max_search_dist = int(model_lats.shape[1]/4)  # Limit on size of grid search
-        closest_point = None
-        j_s, i_s = j, i  # starting points is j, i
-        dj, di = 0, -1
-        # move j_s, i_s in a square spiral centred at j, i
-        while (i_s-i) <= max_search_dist:
-            if ((j_s-j) == (i_s-i)
-                or ((j_s-j) < 0 and (j_s-j) == -(i_s-i))
-                or ((j_s-j) > 0 and (j_s-j) == 1-(i_s-i))
-            ):
-                # Hit the corner of the spiral- change direction
-                dj, di = -di, dj
-            i_s, j_s = i_s+di, j_s+dj  # Take a step to next square
-            if i_s >= 0 and i_s < imax and j_s >= 0 and j_s < jmax and not land_mask[j_s, i_s]:
-                # Found a water point, how close is it?
-                actual_dist = haversine(lon, lat, model_lons[j_s, i_s], model_lats[j_s, i_s])
-                if closest_point is None:
-                    min_dist = actual_dist
-                    closest_point = (j_s, i_s)
-                elif actual_dist < min_dist:
-                    # Keep record of closest point
-                    min_dist = actual_dist
-                    closest_point = (j_s, i_s)
-                # Assumes grids are square- reduces search radius to only
-                # check grids that could potentially be closer than this
-                grid_dist = int(((i_s-i)**2 + (j_s-j)**2)**0.5)
-                if (grid_dist + 1) < max_search_dist:
-                    # Reduce stopping distance for spiral-
-                    # just need to check that no points closer than this one
-                    max_search_dist = grid_dist + 1
-
-        if closest_point is None:
-            raise ValueError(
-                "Only found land points near that coordinate. If land points are OK don't pass in a land_mask."
-            )
-        else:
-            j, i = closest_point
-    return j, i
+    # If point is on land and land mask is provided
+    # try to find closest water point
+    if land_mask is None or not land_mask[j, i]:
+        return j, i
+    try:
+        return _spiral_search_for_closest_water_point(
+            j, i, land_mask, lon, lat, model_lons, model_lats)
+    except ValueError:
+        raise ValueError(
+            'lat/lon on land and no nearby water point found')
--- SalishSeaTools/tests/test_geo_tools.py
+++ SalishSeaTools/tests/test_geo_tools.py
@@ -68,20 +68,73 @@ class TestFindClosestModelPoint:
     """ Unit tests for find_closest_model_point() function
     """
 
-    grid_B = nc.Dataset('/data/nsoontie/MEOPAR/NEMO-forcing/grid/bathy_meter_SalishSea2.nc')
-    bathy, model_lons, model_lats = tidetools.get_bathy_data(grid_B)
+    land_mask = np.array([
+            [False, False, False,  True,  True],
+            [False,  True,  True,  True,  True],
+            [True,  True,  True,  True,  True],
+            [True,  True,  True,  True,  True]
+    ])
 
-    def raises_value_error(self, model_lons, model_lats):
+    model_lons = np.array([
+        [-124.5007019, -124.49552155, -124.49034119, -124.4851532,
+         -124.47997284],
+        [-124.50404358, -124.49885559, -124.49367523, -124.48848724,
+         -124.48330688],
+        [-124.50737762, -124.50219727, -124.49700928, -124.49182892,
+         -124.48664093],
+        [-124.5107193, -124.50553131, -124.50035095, -124.49516296,
+         -124.4899826]
+    ])
+
+    model_lats = np.array([
+        [48.53593826, 48.53788376, 48.53982544, 48.54176712, 48.5437088],
+        [48.53991318, 48.54185486, 48.54379654, 48.54573822, 48.5476799],
+        [48.54388428, 48.54582596, 48.54776764, 48.54970932, 48.551651],
+        [48.54785919, 48.54980087, 48.55174255, 48.55368423, 48.55562592]
+    ])
+
+    def test_raises_value_error(self):
         with pytest.raises(ValueError):
             # lat and lon values that aren't on this grid (0, 0)
-            geo_tools.find_closest_model_point(0, 0, model_lons, model_lats)
+            geo_tools.find_closest_model_point(0, 0, self.model_lons, self.model_lats)
 
     @pytest.mark.parametrize('lon, lat, expected', [
-        (-122, 47.6, (69, 248)),
-        (-124, 48.9, (437, 205)),
-        (-123.515, 48.905, (404, 237)),
-        (-123.50, 48.905, (403, 239))
+        (-124.488, 48.54, (0, 2)),
+        (-124.5, 48.54, (1, 0)),
+        (-124.5, 48.555, (1, 0))
     ])
-    def test_find_closest_model_point(self, lon, lat, expected):
-        j, i = geo_tools.find_closest_model_point(lon, lat, self.model_lons, self.model_lats, land_mask = self.bathy.mask)
-        assert j == expected[0] and i == expected[1]
+    def test_find_water_point(self, lon, lat, expected):
+        j, i = geo_tools.find_closest_model_point(
+            lon, lat, self.model_lons, self.model_lats, land_mask=self.land_mask)
+        assert (j, i) == expected
+
+    def test_no_land_mask_closest_grid_pt_found(self):
+        lon = -124.5
+        lat = 48.555
+        expected = (3, 2)
+        j, i = geo_tools.find_closest_model_point(
+            lon, lat, self.model_lons, self.model_lats)
+        assert (j, i) == expected
+
+    def test_bad_tol_grid_key(self):
+        with pytest.raises(KeyError):
+            # Should raise a key error because the value
+            # for grid is not a key in 'tols'
+            geo_tools.find_closest_model_point(
+                -124.5, 48.5, self.model_lons, self.model_lats, grid="NotAKey")
+
+    def test_no_land_mask_1_grid_pt_found(self):
+        lon, lat = -124.49885559, 48.54185486
+        expected = (1, 1)
+        j, i = geo_tools.find_closest_model_point(
+            lon, lat, self.model_lons, self.model_lats, grid='test',
+            tols={'test': {'tol_lon': 0.000001, 'tol_lat': 0.000001}})
+        assert (j, i) == expected
+
+    def test_no_water_pt_found(self):
+        lon, lat = -124.5, 48.555
+        all_land_land_mask = np.full(self.land_mask.shape, True, dtype=bool)
+        with pytest.raises(ValueError):
+            geo_tools.find_closest_model_point(
+                lon, lat, self.model_lons, self.model_lats,
+                land_mask=all_land_land_mask)
