bc6f201c0ea3e583bbf8026025e9e737f602667d
Ben Moore-Maley <18601938+bmooremaley@users.noreply.github.com>
2016-07-05 15:26:44 -0700

    Added ERDDAP loaders to nc_tools, drifters to data_tools, vector rotation to viz_tools, and filtering to tide_tools

--- SalishSeaTools/salishsea_tools/tidetools.py
+++ SalishSeaTools/salishsea_tools/tidetools.py
@@ -1736,3 +1736,73 @@ def fittit(uaus, time, nconst):
             ap[key] = np.ma.masked_values(ap[key], 0)
 
     return apparam
+
+
+def filter_timeseries(record, winlen=39, method='box'):
+    """Filter a timeseries.
+    
+    Developed for wind and tidal filtering, but can be modified for use
+    with a variety of timeseries data. This function can only operate
+    along the 0 axis. Please modify to include an axis argument in the
+    future.
+    
+    Types of filters (please add to these):
+    * **box**: simple running mean
+    * **doodson**: Doodson bandpass filter (39 winlen required)
+    
+    :arg record: timeseries record to be filtered
+    :type record: :py:class:`numpy.ndarray`, :py:class:`xarray.DataArray`,
+                  or :py:class:`netCDF4.Variable`
+    
+    :arg winlen: window length
+    :type winlen: integer
+    
+    :arg method: type of filter (ex. 'box', 'doodson', etc.)
+    :type method: string
+    
+    :returns filtered: filtered timeseries
+    :rtype: same as record
+    """
+    
+    # Preallocate filtered record
+    filtered = copy.deepcopy(record)
+    
+    # Length along time axis
+    record_length = record.shape[0]
+
+    # Window length
+    w = (winlen - 1) // 2
+    
+    # Construct weight vector
+    weight = np.zeros(w, dtype=int)
+    
+    # Select filter method
+    if method is 'doodson':
+        # Doodson bandpass filter (winlen must be 39)
+        weight[[1, 2, 5, 6, 10, 11, 13, 16, 18]] = 1
+        weight[[0, 3, 8]] = 2
+        centerval = 0
+    elif method is 'box':
+        # Box filter
+        weight[:] = 1
+        centerval = 1
+    else:
+        raise ValueError('Invalid filter method: {}'.format(method))
+    
+    # Loop through record
+    for i in range(record_length):
+        
+        # Adjust window length for end cases
+        W = min(i, w, record_length-i-1)
+        Weight = weight[:W]
+        Weight = np.append(Weight[::-1], np.append(centerval, Weight))
+        Weight = (Weight/sum(Weight, centerval))
+        
+        # Expand weight dims so it can operate on record window
+        for dim in range(record.ndim - 1):
+            Weight = Weight[:, np.newaxis]
+        
+        # Apply mean over window length
+        filtered[i, ...] = np.sum(record[i-W:i+W+1, ...] * Weight, axis=0)
+    
+    return filtered
