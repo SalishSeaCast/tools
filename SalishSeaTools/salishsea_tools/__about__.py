07520e22a086d5e175c70317696a4ec749c60fb7
Doug Latornell <dlatornell@eoas.ubc.ca>
2025-01-08 10:52:38 -0800

    Move version identifier to __about__.py
    Move the version identifier from `__pkg_metadata__.py` to a new `__about__.py`
    file. The version is now dynamically read from that file to maintain the
    consistency of version info. Removed obsolete `__pkg_metadata__.py` symlink.

--- SalishSeaTools/salishsea_tools/__about__.py
+++ SalishSeaTools/salishsea_tools/__about__.py
@@ -1,21 +1,19 @@
 # Copyright 2013-2021 The Salish Sea MEOPAR Contributors
 # and The University of British Columbia
-
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-
+#
 #    https://www.apache.org/licenses/LICENSE-2.0
-
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""SalishSeaTools Package
-"""
-PROJECT = "SalishSeaTools"
-DESCRIPTION = "SalishSeaTools Package"
-VERSION = "24.1.dev0"
-DEV_STATUS = "5 - Production"
+# SPDX-License-Identifier: Apache-2.0
+
+
+__version__ = "24.1.dev0"
