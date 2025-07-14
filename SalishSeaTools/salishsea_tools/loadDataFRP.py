95fabb8cf42520d49eebb0349e966aa6fcfb53c2
Doug Latornell <dlatornell@eoas.ubc.ca>
2025-07-14 11:38:39 -0700

    Fix `SyntaxWarning` and `DeprecationWarning` (#154)
    * Fix deprecated syntax in comparison operations
    
    Replaced `is` and `is not` with `==` and `!=` in string and integer
    comparisons to address `SyntaxWarning` in Python. This ensures compatibility
    with recent Python versions and adheres to the best practices for comparison
    operations.
    
    * Add unit tests for the `formatting_tools` module
    
    Introduced comprehensive tests for the `formatting_tools.format_units()`
    function to validate its handling of valid and invalid inputs. This ensures
    accurate LaTeX unit formatting and robust error handling for unsupported units.
    
    * Fix LaTeX escaping and update string formatting
    
    Corrected LaTeX escaping for degree symbols in unit mappings to ensure
    compatibility with LaTeX rendering and to resolve SyntaxWarning messages.
    Also replaced `.format()` with f-strings for improved readability and
    consistency with modern Python practices.
    
    * Use raw strings in regex patterns to avoid SyntaxWarning
    
    Updated all regex patterns in `loadDataFRP.py` to use raw string literals
    (`r""`). This change prevents `DeprecationWarning` in Python<3.12 and
    `SyntaxWarning` in Python>=3.12 and ensures patterns are interpreted as
    intended.
    
    * Reorder imports in `loadDataFRP.py` for clarity
    
    Organized imports following PEP 8 guidelines, grouping standard libraries,
    third-party modules, and local imports logically. This improves readability
    and maintainability of the code.
    
    * Add copyright and module docstring to `loadDataFRP`
    
    Included copyright information with Apache 2.0 license details and added a
    clear module-level docstring. This ensures proper licensing attribution and
    improves code documentation.

--- SalishSeaTools/salishsea_tools/loadDataFRP.py
+++ SalishSeaTools/salishsea_tools/loadDataFRP.py
@@ -1,13 +1,32 @@
-import numpy as np
-import pandas as pd
-from scipy import signal as ssig
-from scipy import stats as spst
+# Copyright 2013 – present by the SalishSeaCast contributors
+# and The University of British Columbia
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Load CTD cast data from Fraser River plume region that Elise Olson collected to ground-truth
+her turbidity model."""
+
 import os
 import re
 import string
-from salishsea_tools import geo_tools
-import netCDF4 as nc
+
 import gsw
+import netCDF4 as nc
+import numpy as np
+import pandas as pd
+from scipy import signal as ssig
+
+from salishsea_tools import geo_tools
 
 # list CTD cnv files associated with cast numbers
 cnvlist19 = {
@@ -97,8 +116,8 @@ class dataPair:
 
 def fmtVarName(strx):
     """transform string into one that meets python naming conventions"""
-    vName = re.sub("[^a-zA-Z0-9_\-\s/]", "", strx.strip())
-    vName = re.sub("[\s/]", "_", vName)
+    vName = re.sub(r"[^a-zA-Z0-9_\-\s/]", "", strx.strip())
+    vName = re.sub(r"[\s/]", "_", vName)
     vName = re.sub("-", "_", vName)
     if re.match("[0-9]", vName):
         vName = "_" + vName
@@ -183,18 +202,18 @@ def readcnv(fpath):
     alphnumlist = list(string.ascii_letters) + list(string.digits)
     # define regexes for reading headers:
     reSta = re.compile(
-        "(?<=\*\*\sStation:)\s?([0-9])+\s?"
+        r"(?<=\*\*\sStation:)\s?([0-9])+\s?"
     )  # assumes numeric station identifiers
-    reLat = re.compile("(?<=\*\*\sLatitude\s=)\s?([\-0-9\.]+)\s([\-\.0-9]+)\s?([NS])")
-    reLon = re.compile("(?<=\*\*\sLongitude\s=)\s?([\-0-9\.]+)\s([\-\.0-9]+)\s?([EW])")
+    reLat = re.compile(r"(?<=\*\*\sLatitude\s=)\s?([\-0-9\.]+)\s([\-\.0-9]+)\s?([NS])")
+    reLon = re.compile(r"(?<=\*\*\sLongitude\s=)\s?([\-0-9\.]+)\s([\-\.0-9]+)\s?([EW])")
     # start_time = May 08 2002 09:39:10
-    reST = re.compile("(?<=\#\sstart_time\s=).*")
+    reST = re.compile(r"(?<=\#\sstart_time\s=).*")
     # reTZ=re.compile('(?<=\*\*\s...\s\(Time\)\s=).*')
     # reCr=re.compile('(?<=\*\*\sCruise:).*')
-    reNam = re.compile("(?<=\#\sname\s)([0-9]+)\s=\s(.*)\:\s?(.*)\s?")
+    reNam = re.compile(r"(?<=\#\sname\s)([0-9]+)\s=\s(.*)\:\s?(.*)\s?")
 
     # define regex for finding searching:
-    spStart = re.compile("^\s*[0-9]")  # starts with space characters followed by digit
+    spStart = re.compile(r"^\s*[0-9]")  # starts with space characters followed by digit
 
     headers = list()
     # lineno=0
