9f46bc2cc2f6d0d57b6ce9206e564750f671d8a4
Doug Latornell <dlatornell@eoas.ubc.ca>
2026-02-05 12:31:52 -0800

    Add Plotly functionality to bathymetry plotting module
    Introduced new functions to render bathymetry plots using Plotly, providing an
    interactive and visually enhanced alternative to Matplotlib. Includes support for
    tiled subplots, customizable colormaps, and hoverable tooltips showing
    depth and grid cell corrdinates in both lon/lat and x/y.

--- bathymetry/plot_tile.py
+++ bathymetry/plot_tile.py
@@ -43,10 +43,220 @@ bathymetry in both grid index coordinates and geographical coordinates.
 :returns: A tuple with the figure and axes of the plotted bathymetry maps.
 :rtype: tuple
 """
+from cmap import Colormap
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from matplotlib import pyplot as plt
 import matplotlib.cm
 
 from salishsea_tools import viz_tools
+
+
+# Colour map for plotly traces
+colorscale = Colormap("nipy_spectral").to_plotly()
+
+# Hover text template for plotly traces
+hover_tmpl = (
+    "Lon: %{x:.3f}<br>"
+    "Lat: %{y:.3f}<br>"
+    "Depth: %{z:.2f} m<br>"
+    "x, y: %{customdata}"
+    "<extra></extra>"
+)
+
+
+def _add_plotly_panel(
+    fig,
+    x_slices,
+    y_slices,
+    bathy_ds,
+    rez,
+    colorscale=colorscale,
+    zmin=4,
+    zmax=15,
+    hover_tmpl=hover_tmpl,
+    row=1,
+    col=1,
+):
+    """
+    Adds a Plotly heatmap panel to an existing figure using bathymetry dataset information.
+
+    This function extracts slices of bathymetric data, longitude, and latitude from the
+    provided dataset and adds them as a heatmap to the specified subplot of the given figure.
+
+    :param fig: The Plotly figure object to which the heatmap panel will be added.
+    :type fig: plotly.graph_objects.Figure
+
+    :param x_slices: Indices for longitude slices at different resolutions.
+    :type x_slices: dict
+
+    :param y_slices: Indices for latitude slices at different resolutions.
+    :type y_slices: dict
+
+    :param bathy_ds: The Xarray dataset containing bathymetry data. It must include
+                     'Bathymetry', 'nav_lon', and 'nav_lat' variables.
+    :type bathy_ds: xarray.Dataset
+
+    :param rez: Key specifying the resolution level for slicing the data.
+    :type rez: str
+
+    :param colorscale: Colour scale to be used for the heatmap. Defaults to the
+                       value provided in the module scope.
+    :type colorscale: list
+
+    :param zmin: Minimum z-value for the colour scale to normalize the heatmap values.
+                 Defaults to 4.
+    :type zmin: float
+
+    :param zmax: Maximum z-value for the colour scale to normalize the heatmap values.
+                 Defaults to 15.
+    :type zmax: float
+
+    :param hover_tmpl: Custom hover text template for displaying data on hover. Defaults
+                       to the value provided in the module scope.
+    :type hover_tmpl: str
+
+    :param row: The row index of the subplot in the figure where the heatmap is added.
+                Defaults to 1.
+    :type row: int
+
+    :param col: The column index of the subplot in the figure where the heatmap is
+                added. Defaults to 1.
+    :type col: int
+
+    :return: None
+    """
+    tile_bathy = bathy_ds.Bathymetry.isel(y=y_slices[rez], x=x_slices[rez])
+    tile_lons = bathy_ds.nav_lon.isel(y=y_slices[rez], x=x_slices[rez])[0]
+    tile_lats = bathy_ds.nav_lat.isel(y=y_slices[rez], x=x_slices[rez])[0]
+    fig.add_trace(
+        go.Heatmap(
+            z=tile_bathy,
+            y=tile_lats,
+            x=tile_lons,
+            customdata=[[(i, j) for i in tile_bathy.x] for j in tile_bathy.y],
+            zmin=zmin,
+            zmax=zmax,
+            colorscale=colorscale,
+            hovertemplate=hover_tmpl,
+        ),
+        row=row,
+        col=col,
+    )
+
+
+def plotly_tile(
+    x_min_max,
+    y_slices,
+    bathy_ds,
+    dbl_bathy_base_ds,
+    dbl_bathy_ds=None,
+    colorscale=colorscale,
+    zmin=4,
+    zmax=15,
+    hover_tmpl=hover_tmpl,
+):
+    """
+    Generates a Plotly figure to visualize bathymetric data. The
+    resulting figure includes either two or three tiled plots based on whether the adjusted double
+    bathymetry dataset is provided. The single resolution bathymetry and base double resolution bathymetry
+    are always included as the first two panels.
+
+    :param x_min_max: Array containing the minimum and maximum x-coordinates of the single resolution
+                      bathymetry for the tile.
+    :type x_min_max: numpy.ndarray
+
+    :param y_slices: Dictionary containing y-coordinate slices for both single and double
+                     bathymetry data, keyed by ``"sgl"`` and ``"dbl"`` respectively.
+    :type y_slices: dict[str, slice]
+
+    :param bathy_ds: Dataset containing the single resolution bathymetry data.
+    :type bathy_ds: xarray.Dataset
+
+    :param dbl_bathy_base_ds: Dataset containing the base double resolution bathymetry data.
+    :type dbl_bathy_base_ds: xarray.Dataset
+
+    :param dbl_bathy_ds: Dataset containing the adjusted double resolution bathymetry data. Defaults
+                         to None.
+    :type dbl_bathy_ds: xarray.Dataset, optional
+
+    :param colorscale: Colour scale to be used for the heatmap. Defaults to the
+                       value provided in the module scope.
+    :type colorscale: list
+
+    :param zmin: Minimum z-value for the colour scale to normalize the heatmap values.
+                 Defaults to 4.
+    :type zmin: float
+
+    :param zmax: Maximum z-value for the colour scale to normalize the heatmap values.
+                 Defaults to 15.
+    :type zmax: float
+
+    :param hover_tmpl: Custom hover text template for displaying data on hover. Defaults
+                       to the value provided in the module scope.
+    :type hover_tmpl: str
+
+    :return: A Plotly ``Figure`` object containing the configured subplots for bathymetric
+             data visualization.
+    :rtype: plotly.graph_objects.Figure
+    """
+    x_slices = {
+        "sgl": slice(*x_min_max),
+        "dbl": slice(*x_min_max * 2),
+    }
+    subplot_titles = [
+        "bathymetry_202405",
+        "bathymetry_double_202405_base",
+    ]
+    if dbl_bathy_ds is None:
+        cols = 2
+        width = 1600
+    else:
+        cols = 3
+        subplot_titles.append("bathymetry_double_202405")
+        width = 2400
+    fig = make_subplots(rows=1, cols=cols, subplot_titles=subplot_titles)
+    _add_plotly_panel(
+        fig,
+        x_slices,
+        y_slices,
+        bathy_ds,
+        "sgl",
+        col=1,
+        colorscale=colorscale,
+        zmin=zmin,
+        zmax=zmax,
+        hover_tmpl=hover_tmpl,
+    )
+    _add_plotly_panel(
+        fig,
+        x_slices,
+        y_slices,
+        dbl_bathy_base_ds,
+        "dbl",
+        col=2,
+        colorscale=colorscale,
+        zmin=zmin,
+        zmax=zmax,
+        hover_tmpl=hover_tmpl,
+    )
+    if dbl_bathy_ds is not None:
+        _add_plotly_panel(
+            fig,
+            x_slices,
+            y_slices,
+            dbl_bathy_ds,
+            "dbl",
+            col=3,
+            colorscale=colorscale,
+            zmin=zmin,
+            zmax=zmax,
+            hover_tmpl=hover_tmpl,
+        )
+    fig.update_xaxes(tickformat=".3f")
+    fig.update_yaxes(tickformat=".3f")
+    fig.update_layout(height=800, width=width)
+    return fig
 
 
 def plot_tile(
